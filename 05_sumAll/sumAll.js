913330bccd722d07874718d6aec4fccced406aa0
CarlosVega001 <carlos.vega2001@outlook.es>
2023-07-26 22:35:52 +0200

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if(a<0 || b<0) return `ERROR`;
+    if(typeof a===`string` || typeof b===`string`) return `ERROR`;
+    if(isNaN(a) || isNaN(b)) return `ERROR`;
+
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    let i;
+
+    for (i= min; i<=max; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
