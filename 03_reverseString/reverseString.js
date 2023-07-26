28e75f91e8faf17b8e5f11c0c5eaccd84c19ec47
CarlosVega001 <carlos.vega2001@outlook.es>
2023-07-26 17:52:55 +0200

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    string = string.split("");
+    let inverso = [];
+    for(let i= string.length -1; i>=0; i--){
+        inverso.push(string[i]);
+    }
+
+    inverso = inverso.join("");
+    return inverso;
+    
+    
 
 };
+
+reverseString(`Galletitas`)
 
 // Do not edit below this line
 module.exports = reverseString;
