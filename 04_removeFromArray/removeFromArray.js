a674a51e637ad0836787289a4e3cf226946e3738
CarlosVega001 <carlos.vega2001@outlook.es>
2023-07-26 22:15:51 +0200

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    for(let j = 1; j< arguments.length; j++){
+
+            for(let i=0; i<array.length; i++){
+                if (array[i] === arguments[j]) {
+                    //ELIMINAR ARRAY[I]
+                    array.splice(i, 1);
+                }
+            }
+        }
+    return array;
 
 };
 
+removeFromArray([1, 2, 3, 4], 2, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
