9fdaea4702c9f2f8594ea2d50575207e256e3a5e
CarlosVega001 <carlos.vega2001@outlook.es>
2023-07-26 17:29:18 +0200

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repet= ``;
+    if(num<0) return `ERROR`;
+    for(let i=1 ; i <= num; i++){
+        repet += string;
+    }
+    return repet;
 };
 
 // Do not edit below this line
